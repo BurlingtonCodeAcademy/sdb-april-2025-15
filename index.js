2ca746e12f7ad2bdef3bac635cac2bc3c5ed60a0
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-05-21 18:12:41 -0500

    map arrayexample

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ for (let i = 0; i < fruits.length; i++) {
 for (let fruit of fruits) {
     console.log("for of loop: ", fruit)
 }
+
+let numbers = [654, 321, 987, 357, 159, 852]
+
+let numbersDoubled = numbers.map((num) => {
+    return num * 2;
+})
+
+console.log("numbersDoubled: ", numbersDoubled)
